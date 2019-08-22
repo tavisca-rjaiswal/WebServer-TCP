864d00e0e6b0a355878c72414aecb853c463c715
Jaiswal <rjaiswal@internal.tavisca.com>
2019-08-22 17:06:10 +0530

    base code

---
+++ WebServer-TCP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebServer_TCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
